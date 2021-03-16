29551fb6908606826f88634cbccfa85142cc2947
daniel19066 <30605008+daniel19066@users.noreply.github.com>
2021-03-16 02:15:56 +0000

    Deploying to gh-pages from  @ 4816e16ecc7491e803954033e6da299a4cc7b707 🚀

--- docs/sketches/imagen_vergas.js
+++ docs/sketches/imagen_vergas.js
@@ -1,34 +1,51 @@
-
-let img;
 function setup() {
-  createCanvas(512, 512);
-
-  img = createImg("/vc/docs/sketches/lenna.png");
-
+  createCanvas(512*2, 512*2);
+  img=createImg( '/vc/docs/sketches/lenna.png', 'fotito' );
+  
+  
+  img2=createImg( '/vc/docs/sketches/lenna.png', 'fotito2' );
+  
+ 
 }
-  // helper for writing color to array
-
-  function draw() {
-        writeColor(img);
-        console.log(img.height)
-      image(img, 0, 0, width, height);
-  }
 
 
-  function writeColor(image) {
-    let x, y;
-     // fill with random colors
-    for (y = 0; y < img.height; y++) {
-    for (x = 0; x < img.width; x++) {
-      let index = (x + y * width) * 4;
-    var grayscale= 0.33*image.pixels[index]+0.5*image.pixels[index+1]+0.15*image.pixcels[index+2];
-    image.pixels[index] = grayscale;
-    image.pixels[index + 1] = grayscale;
-    image.pixels[index + 2] = grayscale;
-    image.pixels[index + 3] = 255;
-    }
-    
-  }
+function draw() {
+
+ image(img, 0 , 0, 512, 512);
+  img.size(512,512);
+  loadPixels();
+  for (var y = 0; y < height*4; y++) {
+            for (var x = 0; x < width; x++) {
+              var index = (x + y * width)*4;
+              var r = pixels[index+0];
+              var g = pixels[index+1];
+              var b = pixels[index+2];
+
+              var bw = (r + g + b)/3;
+
+              pixels[index+0] = bw;
+              pixels[index+1] = bw;
+              pixels[index+2] = bw;
+        }
+      }
+      updatePixels();
+  image(img2,512,0,512,512);
+  img2.size(512,512);
+  loadPixels();
+  for (var y = 0; y < height*4; y++) {
+    for (var x = 512; x < width; x++) {
+      var index = (x + y * width)*4;
+      var r = pixels[index+0];
+      var g = pixels[index+1];
+      var b = pixels[index+2];
+
+      pixels[index+0] = 255-r;
+      pixels[index+1] = 255-g;
+      pixels[index+2] = 255-b;
+}
+}
+updatePixels();
+}
+
 
   
-  }
