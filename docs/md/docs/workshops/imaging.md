d55b2753755ceb963860ef06b7d24918bfec4503
daniel19066 <dpallaresr@unal.edu.co>
2021-03-15 19:02:50 -0500

    vamos a probar

--- docs/md/docs/workshops/imaging.md
+++ docs/md/docs/workshops/imaging.md
@@ -1,3 +1,4 @@
 # Image and video processing
 
+> :P5  sketch=/docs/sketches/imagen_vergas.js, width=512, height=512
 > :ToCPrevNext
---
+++ docs/sketches/imagen_vergas.js
@@ -0,0 +1,34 @@
+
+
+function setup() {
+  createCanvas(512, 512);
+  let img = createImg();
+  img = loadImage('/vc/docs/sketches/lenna.png');
+  img.hide(); // hide the image in the browser
+  frameRate(1);
+}
+  // helper for writing color to array
+
+  function draw() {
+      writeColor(img)
+      image(img, 0, 0, width, height);
+  }
+
+
+  function writeColor(image) {
+    let x, y;
+     // fill with random colors
+    for (y = 0; y < img.height; y++) {
+    for (x = 0; x < img.width; x++) {
+      let index = (x + y * width) * 4;
+    var grayscale= 0.33*image.pixels[index]+0.5*image.pixels[index+1]+0.15*image.pixcels[index+2];
+    image.pixels[index] = grayscale;
+    image.pixels[index + 1] = grayscale;
+    image.pixels[index + 2] = grayscale;
+    image.pixels[index + 3] = 255;
+    }
+    
+  }
+
+  
+  }
