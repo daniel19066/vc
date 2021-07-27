012ab2bd76fcdc7e7f198dcfea566993f7373230
draco1006 <50886668+draco1006@users.noreply.github.com>
2021-07-27 06:08:40 +0000

    Deploying to gh-pages from  @ 396a2714c11764ac802dad0e7e07ff15b978824d 🚀

--- docs/sketches/Hmosaico.js
+++ docs/sketches/Hmosaico.js
@@ -1,105 +1,70 @@
 let image;
-let om;
-let mosaic;
-let resolution;
-let om_on;
+let mosaico;
+let resolucion;
 
 function preload() {
     //Precargar imagen base y shaders
-    image = loadImage('/vc/docs/sketches/mosaic/lion.jpg');
-    // om = loadImage('/vc/docs/img/omkara.png');
-    mosaic = loadShader('/vc/docs/sketches/mosaico.vert', '/vc/docs/sketches/mosaico.frag');
+    image = loadImage('/vc/docs/sketches/mosaico/lion.jpg');
+    mosaico = loadShader('/vc/docs/sketches/mosaico.vert', '/vc/docs/sketches/mosaico.frag');
 
-    //Cargar imagenes de dataset para mosaico
-    img1 = loadImage('/vc/docs/sketches/mosaic/color1.jpg');
-    img2 = loadImage('/vc/docs/sketches/mosaic/color2.jpg');
-    img3 = loadImage('/vc/docs/sketches/mosaic/color3.jpg');
-    img4 = loadImage('/vc/docs/sketches/mosaic/color4.jpg');
-    img5 = loadImage('/vc/docs/sketches/mosaic/color5.jpg');
-    img6 = loadImage('/vc/docs/sketches/mosaic/color6.jpg');
-    img7 = loadImage('/vc/docs/sketches/mosaic/color7.jpg');
-    img8 = loadImage('/vc/docs/sketches/mosaic/color8.jpg');
-    img9 = loadImage('/vc/docs/sketches/mosaic/color9.jpg');
-    img10 = loadImage('/vc/docs/sketches/mosaic/color10.jpg');
-    img11 = loadImage('/vc/docs/sketches/mosaic/color11.jpg');
-    img12 = loadImage('/vc/docs/sketches/mosaic/color12.jpg');
-    img13 = loadImage('/vc/docs/sketches/mosaic/color13.jpg');
-    img14 = loadImage('/vc/docs/sketches/mosaic/color14.jpg');
-    img15 = loadImage('/vc/docs/sketches/mosaic/color15.jpg');
-    img16 = loadImage('/vc/docs/sketches/mosaic/color16.jpg');
-    img17 = loadImage('/vc/docs/sketches/mosaic/color17.jpg');
-    img18 = loadImage('/vc/docs/sketches/mosaic/color18.jpg');
-    img19 = loadImage('/vc/docs/sketches/mosaic/color19.jpg');
-    img20 = loadImage('/vc/docs/sketches/mosaic/color20.jpg');
-    img21 = loadImage('/vc/docs/sketches/mosaic/color21.jpg');
-    img22 = loadImage('/vc/docs/sketches/mosaic/color22.jpg');
-    img23 = loadImage('/vc/docs/sketches/mosaic/color23.jpg');
-    img24 = loadImage('/vc/docs/sketches/mosaic/color24.jpg');
-    img25 = loadImage('/vc/docs/sketches/mosaic/color25.jpg');
-    img26 = loadImage('/vc/docs/sketches/mosaic/color26.jpg');
-    img27 = loadImage('/vc/docs/sketches/mosaic/color27.jpg');
-
-    
-    
+    //Cargar imagenes de dataset para mosaicoo
+    img1 = loadImage('/vc/docs/sketches/mosaico/color1.jpg');
+    img2 = loadImage('/vc/docs/sketches/mosaico/color2.jpg');
+    img3 = loadImage('/vc/docs/sketches/mosaico/color3.jpg');
+    img4 = loadImage('/vc/docs/sketches/mosaico/color4.jpg');
+    img5 = loadImage('/vc/docs/sketches/mosaico/color5.jpg');
+    img6 = loadImage('/vc/docs/sketches/mosaico/color6.jpg');
+    img7 = loadImage('/vc/docs/sketches/mosaico/color7.jpg');
+    img8 = loadImage('/vc/docs/sketches/mosaico/color8.jpg');
+    img9 = loadImage('/vc/docs/sketches/mosaico/color9.jpg');
+    img10 = loadImage('/vc/docs/sketches/mosaico/color10.jpg');
+    img11 = loadImage('/vc/docs/sketches/mosaico/color11.jpg');
+    img12 = loadImage('/vc/docs/sketches/mosaico/color12.jpg');
+    img13 = loadImage('/vc/docs/sketches/mosaico/color13.jpg');
+    img14 = loadImage('/vc/docs/sketches/mosaico/color14.jpg');
+    img15 = loadImage('/vc/docs/sketches/mosaico/color15.jpg');
 }
 
 function setup() {
     //Crear canvas
-    createCanvas(600, 600, WEBGL);
+    createCanvas(700, 700, WEBGL);
     textureMode(NORMAL);
     noStroke();
-    //definir los shaders a usar
-    shader(mosaic);
-    // mosaic.setUniform('om', om);
-    // //Mostrar en pantalla una casilla de verificación para intercambiar entre el mosaico y la imagen original
-    // om_on = createCheckbox('Mosaic', false);
-    // om_on.changed(() => mosaic.setUniform('om_on', om_on.checked()));
-    // om_on.position(10, 10);
-    //Pasar imagenes a fragment shader
-    mosaic.setUniform('img', image);
-    mosaic.setUniform('img1', img1);
-    mosaic.setUniform('img2', img2);
-    mosaic.setUniform('img3', img3);
-    mosaic.setUniform('img4', img4);
-    mosaic.setUniform('img5', img5);
-    mosaic.setUniform('img6', img6);
-    mosaic.setUniform('img7', img7);
-    mosaic.setUniform('img8', img8);
-    mosaic.setUniform('img9', img9);
-    mosaic.setUniform('img10', img10);
-    mosaic.setUniform('img11', img11);
-    mosaic.setUniform('img12', img12);
-    mosaic.setUniform('img13', img13);
-    mosaic.setUniform('img14', img14);
-    mosaic.setUniform('img15', img15);
     
-    //Mostrar en pantalla un deslizador para ajustar la resolución de la imagen y el mosaico a mostrar
-    resolution = createSlider(10, 100, 30, 1);
-    resolution.position(10, 50);
-    resolution.style('width', '80px');
-    resolution.input(() => mosaic.setUniform('resolution', resolution.value()));
-    mosaic.setUniform('resolution', resolution.value());
+    shader(mosaico);
+
+    mosaico.setUniform('img', image);
+    mosaico.setUniform('img1', img1);
+    mosaico.setUniform('img2', img2);
+    mosaico.setUniform('img3', img3);
+    mosaico.setUniform('img4', img4);
+    mosaico.setUniform('img5', img5);
+    mosaico.setUniform('img6', img6);
+    mosaico.setUniform('img7', img7);
+    mosaico.setUniform('img8', img8);
+    mosaico.setUniform('img9', img9);
+    mosaico.setUniform('img10', img10);
+    mosaico.setUniform('img11', img11);
+    mosaico.setUniform('img12', img12);
+    mosaico.setUniform('img13', img13);
+    mosaico.setUniform('img14', img14);
+    mosaico.setUniform('img15', img15);
+    
+    resolucion = createSlider(10, 100, 50, 1);
+    resolucion.position(90, 50);
+    resolucion.style('width', '90px');
+    resolucion.input(() => mosaico.setUniform('resolucion', resolucion.value()));
+    mosaico.setUniform('resolucion', resolucion.value());
 }
 
 function draw() {
     background(33);
-    
-    cover(true);
-}
-
-function cover(texture = false) {
     beginShape();
-    if (texture) {
-        vertex(-width / 2, -height / 2, 0, 0, 0);
-        vertex(width / 2, -height / 2, 0, 1, 0);
-        vertex(width / 2, height / 2, 0, 1, 1);
-        vertex(-width / 2, height / 2, 0, 0, 1);
-    }
-    else {
-        vertex(-width / 2, -height / 2, 0);
-        vertex(width / 2, -height / 2, 0);
-        vertex(width / 2, height / 2, 0);
-        vertex(-width / 2, height / 2, 0);
-    }
+
+    vertex(-width / 2, -height / 2, 0, 0, 0);
+    vertex(width / 2, -height / 2, 0, 1, 0);
+    vertex(width / 2, height / 2, 0, 1, 1);
+    vertex(-width / 2, height / 2, 0, 0, 1);
+
     endShape(CLOSE);
 }
--- docs/sketches/mosaico.frag
+++ docs/sketches/mosaico.frag
@@ -16,90 +16,56 @@ uniform sampler2D img15;
 
 precision mediump float;
 
-// img (image or video) is sent by the sketch
 uniform sampler2D img;
-// om is sent by the sketch
-uniform sampler2D om;
 
-// toggles om display
-uniform bool om_on;
-// target horizontal & vertical resolution
-uniform float resolution;
+uniform float resolucion;
 
-// interpolated color (same name and type as in vertex shader)
 varying vec4 vVertexColor;
-// interpolated texcoord (same name and type as in vertex shader)
+
 varying vec2 vTexCoord;
 
 void main() {
-  // remap omCoord to [0.0, resolution] ∈ R
-  vec2 omCoord = vTexCoord * resolution;
-  // remap texCoord to [0.0, resolution] ∈ Z
-  vec2 texCoord = floor(omCoord);
-  // remap omCoord to [0.0, 1.0] ∈ R
-  omCoord = omCoord - texCoord;
-  // remap texCoord to [0.0, 1.0] ∈ R
-  texCoord = texCoord * vec2(1.0) / vec2(resolution);
-  // get vec4 image texel (may be used as color hash index by some apps)
+
+  vec2 cPos = vTexCoord * resolucion;
+
+  vec2 texCoord = floor(cPos);
+
+  cPos = cPos - texCoord;
+
+  texCoord = texCoord * vec2(1.0) / vec2(resolucion);
+
   vec4 imgTexel = texture2D(img, texCoord);
 
-  //Calcular luma del texel
   float luma = imgTexel.r *0.299 + imgTexel.g * 0.587 +  imgTexel.b*0.114;
-  //Si está activada la casilla de Mosaico 
-//   if(om_on) {
-   if(true) {
-    vec4 omTexel;
-   //Según el valor del luma calculado, se renderiza la imagen correspondiente
     if(luma < 0.066 && luma >= 0.0){
-         gl_FragColor = texture2D(img1, omCoord);
-
+         gl_FragColor = texture2D(img15, cPos);
     }else if(luma < 0.132 && luma >= 0.066){
-         gl_FragColor = texture2D(img2, omCoord);
-
+         gl_FragColor = texture2D(img2, cPos);
     }else if(luma < 0.198 && luma >= 0.132){
-         gl_FragColor = texture2D(img3, omCoord);
-
+         gl_FragColor = texture2D(img3, cPos);
     }else if(luma < 0.264 && luma >= 0.198){
-         gl_FragColor = texture2D(img4, omCoord);
-
+         gl_FragColor = texture2D(img4, cPos);
     }else if(luma < 0.33 && luma >= 0.264){
-         gl_FragColor = texture2D(img5, omCoord);
-
+         gl_FragColor = texture2D(img5, cPos);
     }else if(luma < 0.396 && luma >= 0.33){
-         gl_FragColor = texture2D(img6, omCoord);
-
+         gl_FragColor = texture2D(img6, cPos);
     }else if(luma < 0.462 && luma >= 0.396){
-         gl_FragColor = texture2D(img7, omCoord);
-
+         gl_FragColor = texture2D(img7, cPos);
     }else if(luma < 0.528 && luma >= 0.462){
-         gl_FragColor = texture2D(img8, omCoord);
-
+         gl_FragColor = texture2D(img8, cPos);
     }else if(luma < 0.594 && luma >= 0.528){
-         gl_FragColor = texture2D(img9, omCoord);
-
+         gl_FragColor = texture2D(img9, cPos);
     }else if(luma < 0.66 && luma >= 0.594){
-         gl_FragColor = texture2D(img10, omCoord);
-
+         gl_FragColor = texture2D(img10, cPos);
     }else if(luma < 0.726 && luma >= 0.66){
-         gl_FragColor = texture2D(img11, omCoord);
-
+         gl_FragColor = texture2D(img11, cPos);
     }else if(luma < 0.792 && luma >= 0.726){
-         gl_FragColor = texture2D(img12, omCoord);
-
+         gl_FragColor = texture2D(img12, cPos);
     }else if(luma < 0.858 && luma >= 0.792){
-         gl_FragColor = texture2D(img13, omCoord);
-
+         gl_FragColor = texture2D(img13, cPos);
     }else if(luma < 0.924 && luma >= 0.858){
-         gl_FragColor = texture2D(img14, omCoord); 
-
+         gl_FragColor = texture2D(img14, cPos); 
     }else if(luma <= 1.0 && luma >= 0.924){
-         gl_FragColor = texture2D(img15, omCoord);
-
+         gl_FragColor = texture2D(img1, cPos);
     }
-
-  }
-  else {
-  //Si no esta acitava la casilla de mosaico, se muestra la imagen original
-    gl_FragColor = imgTexel;
-  }
 }
